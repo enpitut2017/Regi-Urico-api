cdf7cf2ab091b76a71ee764b2411c4eb93e951e2
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-11-27 13:37:29 +0900

    HTTPレスポンスコードを適切なものに修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,11 @@ class ItemsController < ApplicationController
   def destroy
     item_id = params[:item_id]
     event_id = params[:event_id]
-    EventItem.where(item_id: item_id, event_id: event_id).destroy_all
+    r = EventItem.where(item_id: item_id, event_id: event_id).destroy_all
+    if r.empty? # 削除するアイテムがなかったなら、202(accepted)を返す
+      render status: :accepted
+    else # アイテムが削除されたら、204(no_content)を返す
+      render status: :no_content
+    end
   end
 end
