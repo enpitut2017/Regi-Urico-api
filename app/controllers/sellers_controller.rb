c4dd71dec2c1db668d49c7c7002db7454ce14f45
kajyuuen <kajyuuen@gmail.com>
2017-11-27 13:23:38 +0900

    セッションの削除

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -3,7 +3,6 @@ class SellersController < ApplicationController
     @seller = Seller.new(seller_params)
     if @seller.valid?
       @seller.save
-      session[:seller_id] = seller.id
       render json: @seller
     else
       render json: { errors: @seller.errors.messages }
