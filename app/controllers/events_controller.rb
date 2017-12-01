97fade63df3f50f5b6f2370175d408603d98b1fd
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-01 17:51:50 +0900

    DELETE /events/ を実装。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -41,6 +41,25 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    seller = current_seller(request.headers['HTTP_X_AUTHORIZED_TOKEN'])
+    unless seller
+      render json: { errors: 'Unauthorized'}
+    else
+      json_request = JSON.parse(request.body.read)
+      @event = Event.find(json_request['event_id'])
+      @event.destroy
+
+      # 最後に更新されたイベントを返す
+      @event = Event.order('updated_at desc').first
+
+      render json: {
+          id: @event.id,
+          name: @event.name,
+      }
+    end
+  end
+
   def show
     @event = Event.find(params[:id])
     render json: @event, include: {event_items: :item}
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :items, only: [:show, :new, :create]
   resources :events, only: [:index, :show, :create]
   patch '/events', to: 'events#update'
+  delete '/events', to: 'events#destroy'
   resources :register, only: [:create]
   delete '/event_items', to: 'event_items#destroy'
   resources :sellers, only: [:create]
