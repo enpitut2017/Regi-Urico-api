ab8df64f1b51a5cccc93ac6525f984d4b45712ea
Mizuno <tebateba0407@gmail.com>
2017-12-01 17:54:16 +0900

    add post event_id

--- app/controllers/event_items_controller.rb
+++ app/controllers/event_items_controller.rb
@@ -1,4 +1,21 @@
 class EventItemsController < ApplicationController
+  def create
+    json_request = JSON.parse(request.body.read)
+    item = Item.new(name: json_request['item_name'], seller_id: json_request['seller_id'])
+    if item.valid?
+      item.save
+      event_item = EventItem.new(
+        event_id: json_request['event_id'],
+        item_id: item.id,
+        price: json_request['price']
+      )
+      if event_item.valid?
+        event_item.save
+        log = event_item.logs.create(diff_count: json_request['count'])
+      end
+    end
+  end
+
   def destroy
     item_id = params[:item_id]
     event_id = params[:event_id]
@@ -9,4 +26,5 @@ class EventItemsController < ApplicationController
       render status: :no_content
     end
   end
+
 end
