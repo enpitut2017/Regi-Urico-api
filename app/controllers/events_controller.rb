f5a95153180ff43d7329876e1718588c5417352b
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-01 17:09:19 +0900

    POST /events/ を実装。"Authorized"の綴りミスを修正。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,41 @@
 class EventsController < ApplicationController
+  def index
+    seller = current_seller(request.headers['HTTP_X_AUTHORIZED_TOKEN'])
+    unless seller
+      render json: { errors: 'Unauthorized' }
+    else
+      @events = seller.events
+      render json: { event: @events }
+    end
+  end
+
   def create
-    json_request = JSON.parse(request.body.read)
-    Event.create(
-      name: json_request['name'],
-      seller_id: json_request['seller_id']
-    )
+    seller = current_seller(request.headers['HTTP_X_AUTHORIZED_TOKEN'])
+    unless seller
+      render json: { errors: 'Unauthorized'}
+    else
+      json_request = JSON.parse(request.body.read)
+      @event = Event.create(
+          name: json_request['name'],
+          seller_id: seller.id,
+      )
+      render json: @event
+    end
   end
 
   def show
     @event = Event.find(params[:id])
     render json: @event, include: {event_items: :item}
+  end
+
+  private
+
+  def current_seller(token)
+    seller = Seller.find_by(token: token)
+    if seller
+      seller
+    else
+      false
+    end
   end
 end
