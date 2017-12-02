ef542473de61b4ff8bf6f92925e7de32959681e1
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-02 17:10:13 +0900

    存在しないアイテムを削除しようとした時に404 not foundを返すように変更。いつくかの細かいバグを修正。

--- app/controllers/event_items_controller.rb
+++ app/controllers/event_items_controller.rb
@@ -42,42 +42,29 @@ class EventItemsController < ApplicationController
   end
 
   def destroy
-    seller = current_seller(request.headers['HTTP_X_AUTHORIZED_TOKEN'])
-    unless seller
-      render json: {errors: 'Unauthorized'}
-    else
-      json_request = JSON.parse(request.body.read)
-      item_id = json_request['item_id']
-      event_id = json_request['event_id']
+    json_request = JSON.parse(request.body.read)
+    item_id = json_request['item_id']
+    event_id = json_request['event_id']
 
-      # event_itemの削除
-      r = EventItem.where(item_id: item_id, event_id: event_id).destroy_all
-      if r.empty? # 削除するアイテムがなかったなら、202(accepted)を返す
-        render status: :accepted
-      else # アイテムが削除されたら、最新のitemsを返す
-        items = []
-        EventItem.where(event_id: event_id).each do |event_item|
-            item = {
-                item_id: event_item.id,
-                event_id: event_item.event_id,
-                name: Item.find(item_id).name,
-                price: event_item.price,
-            }
-            items.push(item)
-        end
-        render json: {items: items}
-      end
+    # event_itemの削除
+    result = EventItem.where(item_id: item_id, event_id: event_id).destroy_all
+
+    items = []
+    EventItem.where(event_id: event_id).each do |event_item|
+      pp event_item
+      item = {
+          item_id: event_item.item_id,
+          event_id: event_item.event_id,
+          name: Item.find(event_item.item_id).name,
+          price: event_item.price,
+          count: event_item.logs.pluck(:diff_count).sum,
+      }
+      items.push(item)
     end
-  end
-
-  private
-
-  def current_seller(token)
-    seller = Seller.find_by(token: token)
-    if seller
-      seller
-    else
-      false
+    if result.empty? # アイテムが何も削除されなかったなら、最新のitemsとともに404(not found)を返す
+      render json: {items: items}, status: :not_found
+    else # アイテムが削除されたら、最新のitemsを返す
+      render json: {items: items}
     end
   end
 
