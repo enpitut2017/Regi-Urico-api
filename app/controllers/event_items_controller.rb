b1db374b22a9f7b121911d2280ccc505385ab0b4
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-02 16:29:58 +0900

    新規作成に失敗した時に適切にロールバックされるように修正。

--- app/controllers/event_items_controller.rb
+++ app/controllers/event_items_controller.rb
@@ -1,38 +1,44 @@
 class EventItemsController < ApplicationController
+  before_action :current_seller
+
   def create
-    seller = current_seller(request.headers['HTTP_X_AUTHORIZED_TOKEN'])
-    unless seller
-      render json: {errors: 'Unauthorized'}
-    else
-      json_request = JSON.parse(request.body.read)
+    json_request = JSON.parse(request.body.read)
+    event_id = json_request['event_id']
+    price = json_request['price']
+    item_name = json_request['item_name']
+    count = json_request['count']
 
-      # itemの作成
-      item = Item.create(name: json_request['item_name'], seller_id: seller.id)
-      p '!!! item:', item
-      if item.valid? # => true
-        p 'item valid'
-      else
-        p 'item invalid!'
+    begin
+      ActiveRecord::Base.transaction do
+        # itemの作成
+        item = Item.create!(name: item_name, seller_id: @seller.id)
+
+        # event_itemの作成
+        event_item = EventItem.create!(
+            event_id: event_id,
+            item_id: item.id,
+            price: price,
+        )
+
+        # logの作成
+        log = event_item.logs.create!(event_item_id: event_item.id, diff_count: count)
       end
-
-      # event_itemの作成
-      event_item = EventItem.create(
-          event_id: json_request['event_id'],
-          item_id: item.id,
-          price: json_request['price']
-      )
-      p '!!! event_item:', event_item
-      if event_item.valid? # => false
-        p 'event_item is valid'
-      else
-        p 'event_item is invalid!'
-      end
-
-      # logの作成
-      log = event_item.logs.create(event_item_id: event_item.id, diff_count: json_request['count'])
-
-      render json: {items: Item.where(seller_id: seller.id)}
+    rescue ActiveRecord::RecordInvalid => e # 情報不足で新規アイテムが作成できなかった場合
+      return render json: {'errors': e}, status: :bad_request
     end
+
+    items = []
+    EventItem.where(event_id: event_id).each do |event_item|
+      item = {
+          event_id: event_id,
+          item_id: event_item.item_id,
+          name: Item.find(event_item.item_id).name,
+          price: event_item.price,
+          count: event_item.logs.pluck(:diff_count).sum,
+      }
+      items.push(item)
+    end
+    render json: {items: items}
   end
 
   def destroy
@@ -48,12 +54,10 @@ class EventItemsController < ApplicationController
 
   private
 
-  def current_seller(token)
-    seller = Seller.find_by(token: token)
-    if seller
-      seller
-    else
-      false
+  def current_seller()
+    @seller = Seller.find_by(token: request.headers['HTTP_X_AUTHORIZED_TOKEN'])
+    unless @seller
+      render json: {errors: 'Unauthorized'}, status: :unauthorized
     end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
   has_one :event_item
   belongs_to :seller
 end
