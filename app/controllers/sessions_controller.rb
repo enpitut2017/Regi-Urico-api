97652d936fa0bc0004da26918e0381830abf4896
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-04 03:09:47 +0900

    Twitterの認証情報を保存できるように変更。 /auth/twitter?origin=<redirect_url> でTwitter認証後に元のページに戻れるように変更。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,34 @@ class SessionsController < ApplicationController
   def new
     @seller = Seller.find_by(name: login_params[:name])
     if @seller && @seller.authenticate(login_params[:password])
-      render json: @seller
+      render json: {
+          id: @seller.id,
+          token: @seller.token,
+          name: @seller.name,
+          twitter_name: @seller.twitter_name,
+          twitter_screen_name: @seller.twitter_screen_name,
+          twitter_image_url: @seller.twitter_image_url,
+      }
     else
-      render json: { errors: @seller.errors.messages }
+      render json: {errors: @seller.errors.messages}
     end
   end
 
   def callback
+    current_seller
+    render json: {errors: 'Unauthorized'}, status: :unauthorized unless @seller
     auth = auth_hash
-    token = auth.credentials.token
-    secret = auth.credentials.secret
-    p token, secret
+    twitter_info = {
+        twitter_token: auth.credentials.token,
+        twitter_secret: auth.credentials.secret,
+        twitter_id: auth.uid,
+        twitter_name: auth.info.name,
+        twitter_screen_name: auth.info.nickname,
+        twitter_image_url: auth.info.image.sub('_normal', ''),
+    }
+    @seller.update(twitter_info)
+    p @seller
+    redirect_to back_to || '/'
   end
 
   private
@@ -23,5 +40,22 @@ class SessionsController < ApplicationController
 
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def current_seller()
+    @seller = Seller.find_by(token: request.headers['HTTP_X_AUTHORIZED_TOKEN'])
+    unless @seller
+      render json: {errors: 'Unauthorized'}, status: :unauthorized
+    end
+  end
+
+  def back_to
+    if request.env['omniauth.origin'].presence && back_to = CGI.unescape(request.env['omniauth.origin'].to_s)
+      uri = URI.parse(back_to)
+      return back_to if uri.relative? || uri.host == request.host
+    end
+    nil
+  rescue
+    nil
   end
 end
