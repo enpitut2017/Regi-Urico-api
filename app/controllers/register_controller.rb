0b30751b16bf53a5f59b42b9398390e4d0f203dc
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-03 18:19:33 +0900

    エラーハンドリングを改善。

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class RegisterController < ApplicationController
+  before_action :current_seller
+
   def create
     json_request = JSON.parse(request.body.read)
     items = json_request['items']
-    if items.nil?
+    if items.nil? then
       render status: :bad_request
     else
       begin
@@ -12,7 +14,10 @@ class RegisterController < ApplicationController
         ActiveRecord::Base.transaction do
           items.each do |item|
             event_item = EventItem.find_by(event_id: json_request['event_id'], item_id: item['id'])
-            raise ArgumentError if event_item.nil?
+            if event_item.nil? || ! @seller.events.ids.include?(event_item.event_id)
+              raise ArgumentError, "there is no such item, event_id: #{json_request['event_id']}, item_id: #{item['id']}"
+            end
+
             event_item.logs.create(diff_count: item['count'])
             item = Item.find(item['id'])
             tweet = "#{item.seller.name}様の「#{item.name}」は残り#{event_item.logs.sum(:diff_count)}個になりました！"
@@ -24,10 +29,9 @@ class RegisterController < ApplicationController
         tweets.each do |tweet|
           client.update!(tweet)
         end
-        render json: @event, include: { event_items: :item }, status: :created
+        render json: @event, include: {event_items: :item}, status: :created
       rescue => e
-        logger.error e
-        render status: :bad_request
+        render json: {errors: e.message}, status: :bad_request
       end
     end
   end
@@ -40,6 +44,13 @@ class RegisterController < ApplicationController
       config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+  end
+
+  def current_seller()
+    @seller = Seller.find_by(token: request.headers['HTTP_X_AUTHORIZED_TOKEN'])
+    unless @seller
+      render json: {errors: 'Unauthorized'}, status: :unauthorized
     end
   end
 end
