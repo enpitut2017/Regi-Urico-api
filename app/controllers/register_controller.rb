a28d938cc97a770e2ae07f789232aaa67c2c6e8d
TAKAHASHI Shuuji <shuuji3@gmail.com>
2017-12-03 17:21:09 +0900

    1冊につき1ツイートずつツイートするように変更。

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,39 +1,45 @@
+# frozen_string_literal: true
+
 class RegisterController < ApplicationController
   def create
     json_request = JSON.parse(request.body.read)
-    items = json_request["items"]
+    items = json_request['items']
     if items.nil?
       render status: :bad_request
     else
       begin
-        tweet = ""
+        tweets = []
         ActiveRecord::Base.transaction do
           items.each do |item|
-            event_item = EventItem.find_by(event_id: json_request["event_id"], item_id: item["id"])
+            event_item = EventItem.find_by(event_id: json_request['event_id'], item_id: item['id'])
             raise ArgumentError if event_item.nil?
-            event_item.logs.create(diff_count: item["count"])
-            item = Item.find(item["id"])
-            tweet << "\n" << "#{item.seller.name}様の「#{item.name}」は残り#{event_item.logs.sum(:diff_count)}個になりました！"
+            event_item.logs.create(diff_count: item['count'])
+            item = Item.find(item['id'])
+            tweet = "#{item.seller.name}様の「#{item.name}」は残り#{event_item.logs.sum(:diff_count)}個になりました！"
+            tweets.push(tweet)
           end
-          @event = Event.find(json_request["event_id"])
+          @event = Event.find(json_request['event_id'])
         end
         client = get_client
-        client.update!(tweet)
-        render json: @event, include: {event_items: :item}, status: :created
+        tweets.each do |tweet|
+          client.update!(tweet)
+        end
+        render json: @event, include: { event_items: :item }, status: :created
       rescue => e
         logger.error e
         render status: :bad_request
       end
     end
   end
-  
-private
+
+  private
+
   def get_client
     Twitter::REST::Client.new do |config|
-      config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token         = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret  = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
   end
 end
