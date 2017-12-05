2a69e29ea20f4a582fdd469fda4cfd6e49766659
urushiyama <aswif10flis1ntkb@gmail.com>
2017-12-05 18:23:12 +0900

    development環境とtest環境のみクロスドメイン制約をなくすように修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # CORS対策として、開発環境でのクロスオリジン制約を無効化する
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
 end
