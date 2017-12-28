702900baea5535d74105f63975233915e75ec6a1
urushiyama <aswif10flis1ntkb@gmail.com>
2017-12-28 16:53:32 +0900

    新しい仕様に合わせたレスポンスが返るように修正

--- app/controllers/sales_logs_controller.rb
+++ app/controllers/sales_logs_controller.rb
@@ -10,16 +10,15 @@ class SalesLogsController < ApplicationController
       # イベントの所有者とトークンの所有者が異なる
       return render json: { errors: { id: ['is not yours'] }}, status: :forbidden
     else
-      response = {}
+      response = []
       event_sales_logs = event.sales_logs.order('created_at desc')
       if event_sales_logs.empty?
         # 指定したイベントの売上履歴が存在しない
         return render status: :no_content
       end
       sales_logs_by_day = event_sales_logs.group_by{|sales_log| sales_log.created_at.beginning_of_day}
-      detailed = true
-      sales_logs_by_day.each do |day,sales_logs|
-        day_array = []
+      sales_logs_by_day.each do |date, sales_logs|
+        receipts_array = []
         sales_logs.each do |sales_log|
           total = 0
           logs_array = []
@@ -29,19 +28,40 @@ class SalesLogsController < ApplicationController
             logs_array.push({name: log.event_item.item.name, count: count, subtotal: subtotal})
             total += subtotal
           end
-          day_array.push({id: sales_log.id, time: sales_log.created_at.strftime('%H:%M:%S'), total: total, logs: logs_array, detailed: detailed})
-          detailed = false
+          receipts_array.push({
+            id: sales_log.id,
+            time: sales_log.created_at.to_datetime.to_s,
+            formatted_time: sales_log.created_at.strftime('%H:%M:%S'),
+            total: total,
+            logs: logs_array
+          })
         end
-        if day == Date.today
+        if date.to_date == Date.today
           # Today
-          response['today'] = day_array
-        elsif day == Date.yesterday
+          response.push({
+            date: date.to_datetime.to_s,
+            formatted_date: 'Today',
+            receipts: receipts_array
+          })
+        elsif date.to_date == Date.yesterday
           # Yesterday
-          response['yesterday'] = day_array
-        elsif day.year == Date.current.year
-          response[day.strftime('%m/%d')] = day_array
+          response.push({
+            date: date.to_datetime.to_s,
+            formatted_date: 'Yesterday',
+            receipts: receipts_array
+          })
+        elsif date.year == Date.current.year
+          response.push({
+            date: date.to_datetime.to_s,
+            formatted_date: date.strftime('%m/%d'),
+            receipts: receipts_array
+          })
         else
-          response[day.strftime('%Y/%m/%d')] = day_array
+          response.push({
+            date: date.to_datetime.to_s,
+            formatted_date: date.strftime('%Y/%m/%d'),
+            receipts: receipts_array
+          })
         end
       end
       return render json: {sales_logs: response}
